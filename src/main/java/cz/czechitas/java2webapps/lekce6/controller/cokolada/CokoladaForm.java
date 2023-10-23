bcb70c5bf3ff9cfea1a3b1d26adfa2995694f204
Filip Jirsák <filip@jirsak.org>
2023-10-23 22:04:00 +0200

    Doplnění formuláře čokoláda

---
+++ src/main/java/cz/czechitas/java2webapps/lekce6/controller/cokolada/CokoladaForm.java
@@ -0,0 +1,58 @@
+package cz.czechitas.java2webapps.lekce6.controller.cokolada;
+
+import cz.czechitas.java2webapps.lekce6.controller.alkohol.Objem;
+import jakarta.validation.constraints.*;
+
+public class CokoladaForm {
+    @NotBlank
+    private String zbozi;
+    @NotBlank
+    private String jmeno;
+    @Email
+    @NotBlank
+    private String email;
+    @AssertTrue
+    private boolean obchodniPodminky;
+    private boolean newsletter;
+
+    public String getZbozi() {
+        return zbozi;
+    }
+
+    public void setZbozi(String zbozi) {
+        this.zbozi = zbozi;
+    }
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+   public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isObchodniPodminky() {
+        return obchodniPodminky;
+    }
+
+    public void setObchodniPodminky(boolean obchodniPodminky) {
+        this.obchodniPodminky = obchodniPodminky;
+    }
+
+    public boolean isNewsletter() {
+        return newsletter;
+    }
+
+    public void setNewsletter(boolean newsletter) {
+        this.newsletter = newsletter;
+    }
+
+
+}
